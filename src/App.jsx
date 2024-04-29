d4de3784fa254609b4d0ee63824494f27dec045d
valspng <valsapongay@gmail.com>
2024-04-29 22:58:26 +0800

    add graph

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,263 @@
-import { useState } from 'react'
-import './App.css'
-import search from './assets/icons/search.svg'
-import { useStateContext } from './Context'
-import { BackgroundLayout, WeatherCard, MiniCard } from './Components'
+import { useEffect, useState, useCallback } from "react";
+
+import "./App.css";
+import Papa from "papaparse";
+import search from "./assets/icons/search.svg";
+import { useStateContext } from "./Context";
+import { BackgroundLayout, WeatherCard, MiniCard } from "./Components";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import { Select } from "antd";
+import { defaultTo } from "lodash";
+import dayjs from "dayjs";
 
 function App() {
+  const URL = "wss://ggfeiznqqj.execute-api.ap-southeast-1.amazonaws.com/stg";
+  const [input, setInput] = useState("");
+  const { weather, thisLocation, values, place, setPlace } = useStateContext();
 
-  const [input, setInput] = useState('')
-  const { weather, thisLocation, values, place, setPlace } = useStateContext()
-  // console.log(weather)
+  const dateNow = dayjs("2024-03-27").valueOf();
+  const minDate = dayjs("2024-03-27").subtract(7, "days").valueOf();
+  const [currentCO2, setCurrentCO2] = useState("");
+  const [currentPM2, setCurrentPM2] = useState("");
+  const [currentCH2O, setCurrentCH2O] = useState("");
+  const [currentVOC, setCurrentVOC] = useState("");
+  const [dataList, setDataList] = useState([]);
+  const [dataPM2, setDataPM2] = useState([]);
+  const [dataCO2, setDataCO2] = useState([]);
+  const [dataVOC, setDataVOC] = useState([]);
+  const [dataCH2O, setDataCH2O] = useState([]);
+  const [deviceList, setDeviceList] = useState([
+    {
+      value: "eb320d1bf738421780gqaj",
+      label: "eb320d1bf738421780gqaj",
+    },
+    {
+      value: "eb2a60c77fdbab7810k0w6",
+      label: "eb2a60c77fdbab7810k0w6",
+    },
+    {
+      value: "eb10d0a3352b4b3597kboh",
+      label: "eb10d0a3352b4b3597kboh",
+    },
+    {
+      value: "eb17338bd6be497a7e5rm7",
+      label: "eb17338bd6be497a7e5rm7",
+    },
 
-  const submitCity = () => {
-    setPlace(input)
-    setInput('')
-  }
+    {
+      value: "eb30f4bf557caae46bcxdu",
+      label: "eb30f4bf557caae46bcxdu",
+    },
+
+    {
+      value: "ebc59ee9a41d1462fbneut",
+      label: "ebc59ee9a41d1462fbneut",
+    },
+    {
+      value: "ebc9fcf973b273dc4et9xg",
+      label: "ebc9fcf973b273dc4et9xg",
+    },
+    {
+      value: "ebd719fa99091b3aacesra",
+      label: "ebd719fa99091b3aacesra",
+    },
+  ]);
+  const [WSUrl, setWSUrl] = useState(`${URL}/?deviceId=${deviceList[0].value}`);
+  const [selectedId, setSelectedId] = useState("");
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(WSUrl);
+
+  const fetchAllDataList = useCallback(async () => {
+    const data = Papa.parse(await fetchCsv(), {
+      header: true,
+    });
+
+    console.log("[data]", data.data);
+
+    const filterCO2 = data.data.filter(
+      (item) =>
+        item.code === "co2_value" &&
+        item.device_id === deviceList[0].value &&
+        dayjs(item.created_at).valueOf() >= minDate &&
+        dayjs(item.created_at).valueOf() <= dateNow
+    );
+    setDataCO2(filterCO2);
+
+    const filterVOC = data.data.filter(
+      (item) =>
+        item.code === "voc_value" &&
+        item.device_id === deviceList[0].value &&
+        dayjs(item.created_at).valueOf() >= minDate &&
+        dayjs(item.created_at).valueOf() <= dateNow
+    );
+    setDataVOC(filterVOC);
+
+    const filterPM25 = data.data.filter(
+      (item) =>
+        item.code === "pm25_value" &&
+        item.device_id === deviceList[0].value &&
+        dayjs(item.created_at).valueOf() >= minDate &&
+        dayjs(item.created_at).valueOf() <= dateNow
+    );
+    setDataPM2(filterPM25);
+
+    const filterCH2O = data.data.filter(
+      (item) =>
+        item.code === "ch2o_value" &&
+        item.device_id === deviceList[0].value &&
+        dayjs(item.created_at).valueOf() >= minDate &&
+        dayjs(item.created_at).valueOf() <= dateNow
+    );
+    setDataCH2O(filterCH2O);
+
+    setCurrentCO2(filterCO2[0].value);
+    setCurrentPM2(filterPM25[0].value);
+    setCurrentCH2O(filterCH2O[0].value);
+    setCurrentVOC(filterVOC[0].value);
+
+    setDataList(data.data);
+  }, []);
+
+  useEffect(() => {
+    fetchAllDataList();
+  }, []);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const item = JSON.parse(JSON.parse(lastMessage?.data).data.message);
+      console.log("[item]", item);
+
+      switch (item.code) {
+        case "co2_value":
+          setCurrentCO2(item.value);
+          break;
+        case "voc_value":
+          setCurrentVOC(item.value);
+          break;
+        case "ch2o_value":
+          setCurrentCH2O(item.value);
+          break;
+        case "pm25_value":
+          setCurrentPM2(item.value);
+          break;
+        default:
+          console.log(`Sorry, we are out of ${expr}.`);
+      }
+    }
+  }, [lastMessage]);
+
+  const handleClickChangeSocketUrl = useCallback((value) => {
+    console.log("[updated url]", `${URL}/?deviceId=${value}`);
+    setWSUrl(`${URL}/?deviceId=${value}`);
+  }, []);
+
+  const fetchCsv = async () => {
+    const response = await fetch(`src/assets/cbsi_sensor.csv`);
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder("utf-8");
+    const csv = await decoder.decode(result.value);
+    return csv;
+  };
+
+  const handleChangeDevice = (value) => {
+    const filterCO2 = dataList.filter(
+      (item) => item.code === "co2_value" && item.device_id === value
+    );
+    setDataCO2(filterCO2);
+
+    const filterVOC = dataList.filter(
+      (item) => item.code === "voc_value" && item.device_id === value
+    );
+    setDataVOC(filterVOC);
+
+    const filterPM25 = dataList.filter(
+      (item) => item.code === "pm25_value" && item.device_id === value
+    );
+    setDataPM2(filterPM25);
+
+    const filterCH2O = dataList.filter(
+      (item) => item.code === "ch2o_value" && item.device_id === value
+    );
+    setDataCH2O(filterCH2O);
+
+    setCurrentCO2(defaultTo(filterCO2[0]?.value, "N/a"));
+    setCurrentPM2(defaultTo(filterPM25[0]?.value, "N/a"));
+    setCurrentCH2O(defaultTo(filterCH2O[0]?.value, "N/a"));
+    setCurrentVOC(defaultTo(filterVOC[0]?.value, "N/a"));
+
+    setSelectedId(value);
+    handleClickChangeSocketUrl(value);
+  };
 
   return (
-    <div className='w-full h-screen text-white px-8'>
-      <nav className='w-full p-3 flex justify-between items-center'>
-        <h1 className='font-bold tracking-wide text-3xl text-blue-400'>Concepcion Real-Time Indoor Air Quality Monitoring</h1>
-        <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
-          <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
-          <input onKeyUp={(e) => {
-            if (e.key === 'Enter') {
-              // sumit the form
-              submitCity()
-            }
-          }} type="text" placeholder='Search Location' className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)} />
+    <div className="w-full h-screen text-white px-8">
+      <nav className="w-full p-3 flex justify-between items-center">
+        <h1 className="font-bold tracking-wide text-3xl text-blue-400">
+          Concepcion Real-Time Indoor Air Quality Monitoring
+        </h1>
+
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
+          <img src={search} alt="search" className="w-[1.5rem] h-[1.5rem]" />
+          <input
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                // sumit the form
+                submitCity();
+              }
+            }}
+            type="text"
+            placeholder="Search Location"
+            className="focus:outline-none w-full text-[#212121] text-lg"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
         </div>
       </nav>
+
       <BackgroundLayout></BackgroundLayout>
-      <main className='w-full flex flex-wrap gap-8 py-4 px-[10%] items-center justify-center'>
+
+      <main className="w-full flex flex-wrap gap-8 py-4 px-[10%] items-center justify-center">
+        <>
+          <Select
+            placeholder="Select device ID"
+            onChange={handleChangeDevice}
+            options={deviceList}
+          />
+        </>
+
         <WeatherCard
           place={thisLocation}
-          // windspeed={weather.wspd}
-          // humidity={weather.humidity}
           temperature={weather.temp}
           heatIndex={weather.heatindex}
           iconString={weather.conditions}
           conditions={weather.conditions}
+          currentCO2={currentCO2}
+          currentPM2={currentPM2}
+          currentCH2O={currentCH2O}
+          currentVOC={currentVOC}
+          dataPM2={dataPM2}
+          dataCO2={dataCO2}
+          dataVOC={dataVOC}
+          dataCH2O={dataCH2O}
+          selectedId={selectedId}
         />
 
-        <div className='flex justify-center gap-8 flex-wrap w-[60%]'>
-          {
-            values?.slice(1, 7).map(curr => {
-              return (
-                <MiniCard
-                  key={curr.datetime}
-                  time={curr.datetime}
-                  temp={curr.temp}
-                  iconString={curr.conditions}
-                />
-              )
-            })
-          }
+        <div className="flex justify-center gap-8 flex-wrap w-[60%]">
+          {values?.slice(1, 7).map((curr) => {
+            return (
+              <MiniCard
+                key={curr.datetime}
+                time={curr.datetime}
+                temp={curr.temp}
+                iconString={curr.conditions}
+              />
+            );
+          })}
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+
 import { useDate } from "../Utils/useDate";
 import sun from "../assets/icons/sun.png";
 import cloud from "../assets/icons/cloud.png";
@@ -9,7 +11,7 @@ import snow from "../assets/icons/snow.png";
 import storm from "../assets/icons/storm.png";
 import wind from "../assets/icons/windy.png";
 import "../index.css";
-import Papa from "papaparse";
+import { groupBy, maxBy, minBy, reverse } from "lodash";
 
 const WeatherCard = ({
   temperature,
@@ -19,33 +21,170 @@ const WeatherCard = ({
   heatIndex,
   iconString,
   conditions,
+  currentCO2,
+  currentPM2,
+  currentVOC,
+  currentCH2O,
+  dataPM2,
+  dataCO2,
+  dataVOC,
+  dataCH2O,
+  selectedId,
 }) => {
   const [icon, setIcon] = useState(sun);
-  const [dataList, setDataList] = useState([]);
   const [co2List, setCO2List] = useState([]);
-  const [currentCO2, setCurrentCO2] = useState("");
-  const [currentPM2, setCurrentPM2] = useState("");
   const { time } = useDate();
-
-  const fetchAllDataList = useCallback(async () => {
-    const data = Papa.parse(await fetchCsv(), {
-      header: true,
-    });
-
-    const filterCO2 = data.data.filter((item) => item.code === "co2_value");
-    const co2Index = Math.floor(Math.random() * filterCO2.length);
-    const pm2Index = Math.floor(Math.random() * data.data.length);
-
-    setCurrentCO2(filterCO2[co2Index].value);
-    setCurrentPM2(data.data[pm2Index].value);
-
-    setCO2List(filterCO2);
-    setDataList(data.data);
-  }, []);
+  const [chartKeys, setChartKeys] = useState([]);
+  const [chartPM2Keys, setChartPM2Keys] = useState([]);
+  const [chartVOCKeys, setChartVOCKeys] = useState([]);
+  const [chartCH2OKeys, setChartCH2OKeys] = useState([]);
+  const [co2Data, setCO2Data] = useState({
+    data: [],
+    min: 0,
+    max: 0,
+  });
+  const [pm25Data, setPM25Data] = useState({
+    data: [],
+    min: 0,
+    max: 0,
+  });
+  const [vocData, setVOCData] = useState({
+    data: [],
+    min: 0,
+    max: 0,
+  });
+  const [ch2oData, setCH20Data] = useState({
+    data: [],
+    min: 0,
+    max: 0,
+  });
 
   useEffect(() => {
-    fetchAllDataList();
-  }, []);
+    setCO2Data({
+      data: [],
+      min: 0,
+      max: 0,
+    });
+    setPM25Data({
+      data: [],
+      min: 0,
+      max: 0,
+    });
+    setVOCData({
+      data: [],
+      min: 0,
+      max: 0,
+    });
+    setCH20Data({
+      data: [],
+      min: 0,
+      max: 0,
+    });
+  }, [selectedId]);
+
+  useEffect(() => {
+    const groupedCO2 = groupBy(dataCO2, "created_at");
+    const reverseKeys = reverse(Object.keys(groupedCO2));
+    setChartKeys(reverseKeys);
+
+    reverseKeys.forEach((key) => {
+      setCO2Data((prev) => {
+        const maxData = parseInt(
+          maxBy(groupedCO2[key], (item) => item.value).value
+        );
+
+        const max = parseInt(maxBy(dataCO2, (item) => item.value).value);
+        const min = parseInt(minBy(dataCO2, (item) => item.value).value);
+
+        return {
+          data: [...prev.data, maxData],
+          max,
+          min,
+        };
+      });
+    });
+  }, [dataCO2]);
+
+  useEffect(() => {
+    const groupedPM2 = groupBy(dataPM2, "created_at");
+    const reverseKeys = reverse(Object.keys(groupedPM2));
+    setChartPM2Keys(reverseKeys);
+
+    reverseKeys.forEach((key) => {
+      setPM25Data((prev) => {
+        const maxData = parseInt(
+          maxBy(groupedPM2[key], (item) => parseInt(item.value)).value
+        );
+
+        const max = parseInt(
+          maxBy(dataPM2, (item) => parseInt(item.value)).value
+        );
+        const min = parseInt(
+          minBy(dataPM2, (item) => parseInt(item.value)).value
+        );
+
+        return {
+          data: [...prev.data, maxData],
+          max,
+          min,
+        };
+      });
+    });
+  }, [dataPM2]);
+
+  useEffect(() => {
+    const groupedVOC = groupBy(dataVOC, "created_at");
+    const reverseKeys = reverse(Object.keys(groupedVOC));
+    setChartVOCKeys(reverseKeys);
+
+    reverseKeys.forEach((key) => {
+      setVOCData((prev) => {
+        const maxData = parseInt(
+          maxBy(groupedVOC[key], (item) => parseInt(item.value)).value
+        );
+
+        const max = parseInt(
+          maxBy(dataVOC, (item) => parseInt(item.value)).value
+        );
+        const min = parseInt(
+          minBy(dataVOC, (item) => parseInt(item.value)).value
+        );
+
+        return {
+          data: [...prev.data, maxData],
+          max,
+          min,
+        };
+      });
+    });
+  }, [dataVOC]);
+
+  useEffect(() => {
+    const groupedCH2O = groupBy(dataCH2O, "created_at");
+    const reverseKeys = reverse(Object.keys(groupedCH2O));
+    setChartCH2OKeys(reverseKeys);
+
+    reverseKeys.forEach((key) => {
+      setCH20Data((prev) => {
+        const maxData = parseInt(
+          maxBy(groupedCH2O[key], (item) => parseInt(item.value)).value
+        );
+
+        const max = parseInt(
+          maxBy(dataCH2O, (item) => parseInt(item.value)).value
+        );
+        const min = parseInt(
+          minBy(dataCH2O, (item) => parseInt(item.value)).value
+        );
+
+        return {
+          data: [...prev.data, maxData],
+          max,
+          min,
+        };
+      });
+    });
+  }, [dataCH2O]);
 
   useEffect(() => {
     if (iconString) {
@@ -67,44 +206,323 @@ const WeatherCard = ({
     }
   }, [iconString]);
 
-  const fetchCsv = async () => {
-    const response = await fetch(`src/assets/new_file_cbsi_office_sensor.csv`);
-    const reader = response.body.getReader();
-    const result = await reader.read();
-    const decoder = new TextDecoder("utf-8");
-    const csv = await decoder.decode(result.value);
-    return csv;
+  const chartConfig = {
+    type: "bar",
+    height: 150,
+    series: [
+      {
+        name: "CO2",
+        data: [...co2Data.data],
+      },
+    ],
+    options: {
+      chart: {
+        width: "100%",
+        toolbar: {
+          show: false,
+        },
+      },
+      colors: [
+        "#33b2df",
+        "#546E7A",
+        "#d4526e",
+        "#13d8aa",
+        "#A5978B",
+        "#2b908f",
+        "#f9a3a4",
+        "#90ee7e",
+        "#f48024",
+        "#69d2e7",
+      ],
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: false,
+      },
+      plotOptions: {
+        bar: {
+          distributed: true,
+          columnWidth: "95%",
+        },
+      },
+      xaxis: {
+        position: "top",
+        categories: chartKeys,
+      },
+      yaxis: {
+        stepSize: 200,
+        labels: {
+          show: false,
+        },
+        title: {
+          text: "CO2",
+          rotate: 360,
+          offsetX: 5,
+        },
+      },
+      tooltip: {
+        theme: "dark",
+      },
+    },
+  };
+
+  const chartPM2Config = {
+    type: "bar",
+    height: 150,
+    series: [
+      {
+        name: "PM2.5",
+        data: [...pm25Data.data],
+      },
+    ],
+    options: {
+      chart: {
+        width: "100%",
+        toolbar: {
+          show: false,
+        },
+      },
+      colors: [
+        "#33b2df",
+        "#546E7A",
+        "#d4526e",
+        "#13d8aa",
+        "#A5978B",
+        "#2b908f",
+        "#f9a3a4",
+        "#90ee7e",
+        "#f48024",
+        "#69d2e7",
+      ],
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: false,
+      },
+      plotOptions: {
+        bar: {
+          distributed: true,
+          columnWidth: "95%",
+        },
+      },
+      xaxis: {
+        position: "top",
+        categories: chartPM2Keys,
+      },
+      yaxis: {
+        stepSize: 10,
+        labels: {
+          show: false,
+        },
+        title: {
+          text: "PM2.5",
+          rotate: 360,
+          offsetX: 5,
+        },
+      },
+      tooltip: {
+        theme: "dark",
+      },
+    },
+  };
+
+  const chartVOCConfig = {
+    type: "bar",
+    height: 150,
+    series: [
+      {
+        name: "VOC",
+        data: [...vocData.data],
+      },
+    ],
+    options: {
+      chart: {
+        width: "100%",
+        toolbar: {
+          show: false,
+        },
+      },
+      colors: [
+        "#33b2df",
+        "#546E7A",
+        "#d4526e",
+        "#13d8aa",
+        "#A5978B",
+        "#2b908f",
+        "#f9a3a4",
+        "#90ee7e",
+        "#f48024",
+        "#69d2e7",
+      ],
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: false,
+      },
+      plotOptions: {
+        bar: {
+          distributed: true,
+          columnWidth: "95%",
+        },
+      },
+      xaxis: {
+        position: "top",
+        categories: chartVOCKeys,
+      },
+      yaxis: {
+        stepSize: 40,
+        labels: {
+          show: false,
+        },
+        title: {
+          text: "VOC",
+          rotate: 360,
+          offsetX: 5,
+        },
+      },
+      tooltip: {
+        theme: "dark",
+      },
+    },
+  };
+
+  const chartCH2OConfig = {
+    type: "bar",
+    height: 150,
+    series: [
+      {
+        name: "CH2O",
+        data: [...ch2oData.data],
+      },
+    ],
+    options: {
+      chart: {
+        width: "100%",
+        toolbar: {
+          show: false,
+        },
+      },
+      colors: [
+        "#33b2df",
+        "#546E7A",
+        "#d4526e",
+        "#13d8aa",
+        "#A5978B",
+        "#2b908f",
+        "#f9a3a4",
+        "#90ee7e",
+        "#f48024",
+        "#69d2e7",
+      ],
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: false,
+      },
+      plotOptions: {
+        bar: {
+          distributed: true,
+          columnWidth: "95%",
+        },
+      },
+      xaxis: {
+        position: "top",
+        categories: chartCH2OKeys,
+      },
+      yaxis: {
+        stepSize: 10,
+        labels: {
+          show: false,
+        },
+        title: {
+          text: "CH2O",
+          rotate: 360,
+          offsetX: 5,
+        },
+      },
+      tooltip: {
+        theme: "dark",
+      },
+    },
   };
 
   return (
-    <div className="w-[22rem] min-w-[22rem] h-[30rem] glassCard p-4">
+    <div className="glassCard p-4 w-screen">
       <div className="flex w-full just-center, items-center gap-4 mt-12 mb-4">
         <img src={icon} alt="weather_icon" />
         <p className="font-bold text-5xl flex justify-center items-center">
           {temperature} &deg;C
         </p>
       </div>
+
       <div className="font-bold text-center text-xl">{place}</div>
+
       <div className="w-full flex justify-between items-center mt-4">
         <p className="flex-1 text-center p-2">{new Date().toDateString()}</p>
         <p className="flex-1 text-center p-2">{time}</p>
       </div>
-      <div className="w-full flex justify-between items-center mt-4 gap-4">
+
+      <div className="w-full flex justify-between items-center mt-4 gap-2">
         <p className="flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg">
           CO2 {currentCO2}
           <p className="font-normal">{windspeed} ppm</p>
         </p>
+
         <p className="flex-1 text-center p-2 font-bold rounded-lg bg-green-600">
           PM {currentPM2} <p className="font-normal">{humidity} ppm</p>
         </p>
       </div>
+
       <div className="w-full p-3 mt-4 flex justify-between items-center">
-        <p className="font-semibold text-lg">Heat Index</p>
-        <p className="text-lg">{heatIndex ? heatIndex : "N/A"}</p>
+        <p className="font-semibold text-lg">CH2O(Formaldehyde)</p>
+        <p className="text-lg">{currentCH2O ? currentCH2O : "N/A"}</p>
       </div>
+
+      <div className="w-full p-3 mt-4 flex justify-between items-center">
+        <p className="font-semibold text-lg">VOC</p>
+        <p className="text-lg">{currentVOC ? currentVOC : "N/A"}</p>
+      </div>
+
       <hr className="bg-slate-600" />
-      <div className="w-full p-4 flex justify-center items-center text-3xl font-semibold">
-        {conditions}
+
+      <div className="w-full mt-2 mb-0 flex">
+        <ReactApexChart {...chartConfig} className="basis-5/6" />
+        <div className="flex flex-col justify-center basis-1/6">
+          <p className="font-semibold text-lg text-rose-600">{co2Data.max}</p>
+          <p className="font-semibold text-lg text-yellow-700">{co2Data.min}</p>
+        </div>
+      </div>
+
+      <div className="w-full mb-0 flex">
+        <ReactApexChart {...chartPM2Config} className="basis-5/6" />
+        <div className="flex flex-col justify-center basis-1/6">
+          <p className="font-semibold text-lg text-rose-600">{pm25Data.max}</p>
+          <p className="font-semibold text-lg text-yellow-700">
+            {pm25Data.min}
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full mb-0 flex">
+        <ReactApexChart {...chartVOCConfig} className="basis-5/6" />
+        <div className="flex flex-col justify-center basis-1/6">
+          <p className="font-semibold text-lg text-rose-600">{vocData.max}</p>
+          <p className="font-semibold text-lg text-yellow-700">{vocData.min}</p>
+        </div>
+      </div>
+
+      <div className="w-full mb-0 flex">
+        <ReactApexChart {...chartCH2OConfig} className="basis-5/6" />
+        <div className="flex flex-col justify-center basis-1/6">
+          <p className="font-semibold text-lg text-rose-600">{ch2oData.max}</p>
+          <p className="font-semibold text-lg text-yellow-700">
+            {ch2oData.min}
+          </p>
+        </div>
       </div>
     </div>
   );
